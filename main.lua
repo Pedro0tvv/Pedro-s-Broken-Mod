2895fe47dcbea99a0f7b6fda8c328ebc9bc628a1
Pedro0tvv <andrey.tolini80@gmail.com>
2026-02-08 02:43:26 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -36,7 +36,7 @@ local NFS = require("nativefs")
 to_big = to_big or function(a) return a end
 lenient_bignum = lenient_bignum or function(a) return a end
 
-local jokerIndexList = {4,1,3,5,2,6}
+local jokerIndexList = {5,1,3,6,2,7,4}
 
 local function load_jokers_folder()
     local mod_path = SMODS.current_mod.path
@@ -111,6 +111,7 @@ SMODS.ObjectType({
     cards = {
         ["j_pedrosbr_kristel"] = true,
         ["j_pedrosbr_myssyli"] = true,
+        ["j_pedrosbr_nouri"] = true,
         ["j_pedrosbr_randall"] = true,
         ["j_pedrosbr_tayeb"] = true
     },
