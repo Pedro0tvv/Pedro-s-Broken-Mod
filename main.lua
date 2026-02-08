f0a1ef36bca9bcfc9dbc150e10c6e1cd7c620c6d
Pedro0tvv <andrey.tolini80@gmail.com>
2026-02-07 20:19:22 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -24,11 +24,19 @@ SMODS.Atlas({
     atlas_table = "ASSET_ATLAS"
 })
 
+SMODS.Atlas({
+    key = "CustomConsumables", 
+    path = "CustomConsumables.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
 local NFS = require("nativefs")
 to_big = to_big or function(a) return a end
 lenient_bignum = lenient_bignum or function(a) return a end
 
-local jokerIndexList = {3,1,2,4}
+local jokerIndexList = {4,1,3,5,2,6}
 
 local function load_jokers_folder()
     local mod_path = SMODS.current_mod.path
@@ -38,6 +46,32 @@ local function load_jokers_folder()
         local file_name = files[jokerIndexList[i]].name
         if file_name:sub(-4) == ".lua" then
             assert(SMODS.load_file("jokers/" .. file_name))()
+        end
+    end
+end
+
+
+local consumableIndexList = {1}
+
+local function load_consumables_folder()
+    local mod_path = SMODS.current_mod.path
+    local consumables_path = mod_path .. "/consumables"
+    local files = NFS.getDirectoryItemsInfo(consumables_path)
+    local set_file_number = #files + 1
+    for i = 1, #files do
+        if files[i].name == "sets.lua" then
+            assert(SMODS.load_file("consumables/sets.lua"))()
+            set_file_number = i
+        end
+    end    
+    for i = 1, #consumableIndexList do
+        local j = consumableIndexList[i]
+        if j >= set_file_number then 
+            j = j + 1
+        end
+        local file_name = files[j].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("consumables/" .. file_name))()
         end
     end
 end
@@ -60,6 +94,7 @@ end
 
 load_rarities_file()
 load_jokers_folder()
+load_consumables_folder()
 SMODS.ObjectType({
     key = "pedrosbr_food",
     cards = {
@@ -85,8 +120,10 @@ SMODS.ObjectType({
 SMODS.ObjectType({
     key = "pedrosbr_pedrosbr_jokers",
     cards = {
+        ["j_pedrosbr_kristel"] = true,
         ["j_pedrosbr_myssyli"] = true,
-        ["j_pedrosbr_randall"] = true
+        ["j_pedrosbr_randall"] = true,
+        ["j_pedrosbr_tayeb"] = true
     },
 })
 
