26c1e3e474f38e6dce3dadebadfe1480eed8254c
Pedro0tvv <andrey.tolini80@gmail.com>
2026-03-08 12:08:06 -0400

    Add files via upload

---
+++ enhancements/naposm.lua
@@ -0,0 +1,23 @@
+
+SMODS.Enhancement {
+    key = 'naposm',
+    pos = { x = 0, y = 0 },
+    loc_txt = {
+        name = 'Naposm',
+        text = {
+            [1] = 'When this {C:attention}Card{} is',
+            [2] = '{C:attention}Scored{} give {X:edition,C:red}^2{} and {X:edition,C:blue}^2{}',
+            [3] = '{C:green}1 in 1000{} Chance of not {C:attention}Scoring{}'
+        }
+    },
+    atlas = 'CustomEnhancements',
+    any_suit = false,
+    replace_base_card = false,
+    no_rank = false,
+    no_suit = false,
+    always_scores = false,
+    unlocked = true,
+    discovered = true,
+    no_collection = false,
+    weight = 5
+}
