ed79f9795f3cc3a8e97bd4b0beceff4c9c0431cf
Pedro0tvv <andrey.tolini80@gmail.com>
2026-03-10 21:51:45 -0400

    Add files via upload

---
+++ consumables/naposm.lua
@@ -0,0 +1,80 @@
+
+SMODS.Consumable {
+    key = 'naposm',
+    set = 'Spectral',
+    pos = { x = 0, y = 0 },
+    loc_txt = {
+        name = 'Naposm',
+        text = {
+            [1] = 'Select {C:attention}2{} {C:attention}Cards{} To {C:enhanced}Enhance{}',
+            [2] = 'To {X:dark_edition,C:common}Naposm{}'
+        }
+    },
+    cost = 10,
+    unlocked = true,
+    discovered = true,
+    hidden = false,
+    can_repeat_soul = false,
+    atlas = 'CustomConsumables',
+    use = function(self, card, area, copier)
+        local used_card = copier or card
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.4,
+            func = function()
+                play_sound('tarot1')
+                used_card:juice_up(0.3, 0.5)
+                return true
+            end
+        }))
+        for i = 1, #G.hand.highlighted do
+            local percent = 1.15 - (i - 0.999) / (#G.hand.highlighted - 0.998) * 0.3
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.15,
+                func = function()
+                    G.hand.highlighted[i]:flip()
+                    play_sound('card1', percent)
+                    G.hand.highlighted[i]:juice_up(0.3, 0.3)
+                    return true
+                end
+            }))
+        end
+        delay(0.2)
+        for i = 1, #G.hand.highlighted do
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.1,
+                func = function()
+                    G.hand.highlighted[i]:set_ability(G.P_CENTERS['m_pedrosbr_naposm'])            
+                    return true
+                end
+            }))
+        end
+        for i = 1, #G.hand.highlighted do
+            local percent = 0.85 + (i - 0.999) / (#G.hand.highlighted - 0.998) * 0.3
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.15,
+                func = function()
+                    G.hand.highlighted[i]:flip()
+                    play_sound('tarot2', percent, 0.6)
+                    G.hand.highlighted[i]:juice_up(0.3, 0.3)
+                    return true
+                end
+            }))
+        end
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.2,
+            func = function()
+                G.hand:unhighlight_all()
+                return true
+            end
+        }))
+        delay(0.5)
+    end,
+    can_use = function(self, card)
+        return true
+    end
+}
